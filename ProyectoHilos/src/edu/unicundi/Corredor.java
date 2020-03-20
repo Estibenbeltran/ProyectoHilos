7f6e625b629b9e2c5ee55582ebaec1df59bd1781
Dairo Beltran <daiydan99@hotmail.com>
2020-03-19 19:05:46 -0500

    correccion

--- ProyectoHilos/src/edu/unicundi/Corredor.java
+++ ProyectoHilos/src/edu/unicundi/Corredor.java
@@ -5,57 +5,120 @@
  */
 package edu.unicundi;
 
+import java.time.LocalTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Random;
+import javax.swing.JOptionPane;
+
 
 /**
  *
  * @author daiyd
  */
-public class Corredor extends Thread{
-    private String nombre;
-    private int posicion;
-    private ComunEquipo comun;
+public class Corredor extends Thread {
+    Panel objPanelGlobal;
+    int salida;
+    Ventana ventana;
+    static boolean ganador = false;
+    static boolean llegada1 = false, llegada2 = false,llegada3 = false;
 
-    public Corredor(String nombre, int posicion, ComunEquipo comun) {
-        this.nombre = nombre;
-        this.posicion = posicion;
-        this.comun = comun;
+    Corredor(Panel objPanel, int posicion, Ventana v){
+        objPanelGlobal = objPanel;
+        salida = posicion;
+        ventana = v;
     }
-
     @Override
-    public void run() {
-
-        if (posicion == 1) {
-            System.out.println("Arranco a correr " + nombre);
-            for (int i = 0; i < 33; i++) {
-         
-                try {
-                    Thread.sleep(100);
-                    System.out.println(nombre + " Voy en el paso: " + i);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
-                }
-               
-            }
-            
-            synchronized(comun){
-                comun.notifyAll();
-            }
-            
-            System.out.println("Corredor " + nombre + " Acabo");
-        } else {
-            synchronized(comun) {
-                System.out.println("Corredor " + nombre + " en espera");
-                try {
-                    comun.wait();
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
-                }                
-            }
-            System.out.println("Corredor " + nombre + " empiezo a correr");
+    public void run(){
+        ganador = false;
+        int ale1, ale2, ale3;
+        boolean jugar = false;
+        ale1 = (int)(Math.random()*140)+81;
+        ale2 = (int)(Math.random()*140)+81;
+        ale3 = (int)(Math.random()*140)+81;
+        super.run(); 
+        if(salida>=1 && salida<=3){
+            jugar = true;
         }
+        if(jugar){
+            for(int i=0; i<368; i++){
+                if(salida == 1){
+                    if(objPanelGlobal.x1<368){
+                        objPanelGlobal.x1+=2;
+                    }else{
+                        llegada1 = true;
+                        Ventana v = new Ventana();
+                        i=369;
+                        if(ganador==false){
+                        JOptionPane.showMessageDialog(v, "GANA LA EL CORREDOR 1", "Ganador", JOptionPane.INFORMATION_MESSAGE);
+                        ganador=true;
+                        }
+                    }
 
+                    try {
+                        Thread.sleep(ale1);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    objPanelGlobal.repaint();
+                    objPanelGlobal.removeAll();
+                }
+
+                if(salida == 2){
+                    if(objPanelGlobal.x2<368){
+                        objPanelGlobal.x2+=2;
+                    }else{
+                        llegada2 = true;
+                        Ventana v = new Ventana();
+                        i = 369;
+                        if(ganador==false){
+                        JOptionPane.showMessageDialog(v, "GANA EL CORREDOR 2", "Ganador", JOptionPane.INFORMATION_MESSAGE);
+                        ganador=true;
+                        }
+                    }
+
+                    try {
+                        Thread.sleep(ale2);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    objPanelGlobal.repaint();
+                    objPanelGlobal.removeAll();
+                }
+                
+
+                if(salida == 3){
+                    if(objPanelGlobal.x3<368){
+                        objPanelGlobal.x3+=2;
+                    }else{
+                        llegada3 = true;
+                        
+                        Ventana v = new Ventana();
+                        i = 369;
+                        if(ganador==false){
+                        JOptionPane.showMessageDialog(v, "GANA EL CORREDOR 3", "Ganador", JOptionPane.INFORMATION_MESSAGE);
+                        ganador=true;
+                        }
+                    }
+
+                    try {
+                        Thread.sleep(ale3);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    objPanelGlobal.repaint();
+                    objPanelGlobal.removeAll();
+                }
+            }
+        }else{
+            objPanelGlobal.x1=0;
+            objPanelGlobal.x2=0;
+            objPanelGlobal.x3=0;
+            
+            objPanelGlobal.repaint();
+            objPanelGlobal.removeAll();
+        }
     }
-
 }
